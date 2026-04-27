dba567890bc06a1e85efd6b1d7333636674e7315
howardshan <144498331+howardshan@users.noreply.github.com>
2026-04-27 14:38:48 -0500

    042701
    1.修改所有的logo
    2.修复所有的法律文书

--- dealjoy_merchant/android/app/build.gradle.kts
+++ dealjoy_merchant/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.dealjoy.dealjoy_merchant"
+    namespace = "com.crunchyplum.crunchyplum_merchant"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.dealjoy.dealjoy_merchant"
+        applicationId = "com.crunchyplum.crunchyplum_merchant"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
